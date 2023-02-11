15266985b931e5a0ab40f16cc561eb94b62c9acb
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-11 20:43:56 +0530

    Add styling for checkout form

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
-      cartItems: [],
+      cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [],
       size: '',
       sort: ''
     };
@@ -29,11 +29,17 @@ class App extends React.Component {
     }
 
     this.setState({ cartItems });
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
   };
 
   removeFromCart = (product) => {
     const cartItems = this.state.cartItems.slice();
     this.setState({ cartItems: cartItems.filter((x) => x._id !== product._id) });
+    localStorage.setItem('cartItems', JSON.stringify(cartItems.filter((x) => x._id !== product._id)));
+  };
+
+  createOrder = (order) => {
+    alert('Need to save order for ' + order.name);
   };
 
   sortProducts = (event) => {
@@ -88,7 +94,11 @@ class App extends React.Component {
               <Products products={this.state.products} addToCart={this.addToCart} />
             </div>
             <div className="sidebar">
-              <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} />
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+                createOrder={this.createOrder}
+              />
             </div>
           </div>
         </main>
