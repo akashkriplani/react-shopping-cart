6a9f0e28c8498b61a8994b44645cf81d4242fd11
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-13 23:23:43 +0530

    Add a link for admin screen to App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends React.Component {
           <div className="grid-container">
             <header>
               <Link to="/">React Shopping Cart</Link>
+              <Link to="/admin">Admin</Link>
             </header>
             <main>
               <Routes>
